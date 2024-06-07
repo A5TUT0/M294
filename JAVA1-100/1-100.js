6f1e0362aae741d384f3fdfb6a258df6c4ed3857
A5TUT0 <noahlezama2000@gmail.com>
2024-06-07 14:03:50 +0200

    Java 1 bis 100 durch 7

---
+++ JAVA1-100/1-100.js
@@ -0,0 +1,10 @@
+function imprimirnumber() {
+  for (let index = 1; index <= 100; index++) {
+    if (index / 7) {
+      alert(index);
+    } else {
+      alert();
+    }
+  }
+}
+imprimirnumber();
